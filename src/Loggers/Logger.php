d5970f091c07082ca63feb8530629921ee22f5f9
Oleksandr Moik <sashamoik2016@gmail.com>
2022-12-20 15:31:49 +0200

    fix phpstan errors

--- src/Loggers/Logger.php
+++ src/Loggers/Logger.php
@@ -8,7 +8,7 @@ interface Logger
 {
     public function init(): void;
 
-    public function line(?string $content = ''): void;
+    public function line(?string $content = '', ?string $style = null): void;
 
     public function newSection(int $step, string $name, int $total): void;
 }
