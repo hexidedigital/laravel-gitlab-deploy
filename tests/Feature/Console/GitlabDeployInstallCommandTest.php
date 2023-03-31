b228dfe87a1da8a05c9dfcc4330ed68c0d033b23
Oleksandr Moik <50796878+Oleksandr-Moik@users.noreply.github.com>
2023-03-31 12:14:51 +0300

    Fix editing gitignore file for install command (#20)

--- tests/Feature/Console/GitlabDeployInstallCommandTest.php
+++ tests/Feature/Console/GitlabDeployInstallCommandTest.php
@@ -9,9 +9,16 @@ function safeUnlink(string $path): void
     }
 }
 
+beforeEach(function () {
+    safeUnlink(config_path('gitlab-deploy.php'));
+});
+
+afterEach(function () {
+    safeUnlink(config_path('gitlab-deploy.php'));
+});
+
 test('the install command copies the configuration', function () {
     // make sure we're starting from a clean state
-    safeUnlink(config_path('gitlab-deploy.php'));
     assertFalse(File::isFile(config_path('gitlab-deploy.php')));
 
     $this->artisan('gitlab-deploy:install')
@@ -38,8 +45,6 @@ test('when a config file is present users can choose to not overwrite it', funct
         ->assertSuccessful();
 
     assertEquals('initial content', File::get(config_path('gitlab-deploy.php')));
-
-    safeUnlink(config_path('gitlab-deploy.php'));
 });
 
 test('when a config file is present users can choose to overwrite it', function () {
@@ -64,8 +69,6 @@ test('when a config file is present users can choose to overwrite it', function 
         File::get(__DIR__ . '/../../../config/gitlab-deploy.php'),
         File::get(config_path('gitlab-deploy.php'))
     );
-
-    safeUnlink(config_path('gitlab-deploy.php'));
 });
 
 test('the install command copies sample files', function (string $sampleFile) {
@@ -86,3 +89,68 @@ test('the install command copies sample files', function (string $sampleFile) {
     '.deploy/deploy-prepare.yml',
     'deploy.php',
 ]);
+
+test('adds ssh and work folders to root .gitignore file', function () {
+    File::put(base_path('.gitignore'), implode(PHP_EOL, ["/vendor", "/.idea"]));
+
+    config([
+        'gitlab-deploy.config-file' => base_path('.deploy/deploy-prepare.yml'),
+        'gitlab-deploy.ssh.folder' => base_path('.ssh/{{STAGE}}'),
+    ]);
+
+    $command = $this->artisan('gitlab-deploy:install')
+        ->assertSuccessful();
+
+    $command->execute();
+
+    $command
+        ->expectsOutput('Updating: .gitignore file')
+        ->expectsOutput('Updated: .gitignore file');
+
+    assertEquals(
+        implode(PHP_EOL, ["/vendor", "/.idea", "/.ssh", "/.deploy"]),
+        File::get(base_path('.gitignore'))
+    );
+
+    safeUnlink(base_path('.gitignore'));
+});
+
+test('do not edit when ssh and work folders already in root .gitignore file', function () {
+    File::put(base_path('.gitignore'), implode(PHP_EOL, ["/vendor", "/.idea", "/.ssh", "/.deploy"]));
+
+    config([
+        'gitlab-deploy.config-file' => base_path('.deploy/deploy-prepare.yml'),
+        'gitlab-deploy.ssh.folder' => base_path('.ssh/{{STAGE}}'),
+    ]);
+
+    $command = $this->artisan('gitlab-deploy:install')
+        ->assertSuccessful();
+
+    $command->execute();
+
+    $command
+        ->expectsOutput('Updating: .gitignore file')
+        ->expectsOutput('No need to update.')
+        ->doesntExpectOutput('Updated: .gitignore file');
+
+    assertEquals(
+        implode(PHP_EOL, ["/vendor", "/.idea", "/.ssh", "/.deploy"]),
+        File::get(base_path('.gitignore'))
+    );
+
+    safeUnlink(base_path('.gitignore'));
+});
+
+test('do nothing when in project missing .gitignore file', function () {
+    safeUnlink(base_path('.gitignore'));
+    assertFalse(File::isFile(base_path('.gitignore')));
+
+    $command = $this->artisan('gitlab-deploy:install')
+        ->assertSuccessful();
+
+    $command->execute();
+
+    $command->doesntExpectOutput('Updating: .gitignore file');
+
+    assertFalse(File::isFile(base_path('.gitignore')));
+});
