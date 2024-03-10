36cc90e462d3cc365719dd7d7b8d51390a8329d2
Oleksandr Moik <sashamoik2016@gmail.com>
2024-03-10 22:21:15 +0200

    wip: add ConfigurationBuilderTest

---
+++ tests/Feature/Configurations/ConfigurationBuilderTest.php
@@ -0,0 +1,24 @@
+<?php
+
+use HexideDigital\GitlabDeploy\Helpers\Builders\ConfigurationBuilder;
+use HexideDigital\GitlabDeploy\Helpers\ParseConfiguration;
+
+it('parses configuration', function () {
+    $parser = new ParseConfiguration();
+
+    $path = dump(config('gitlab-deploy.config-file'));
+
+    $fileData = $parser->parseFile($path);
+
+    dump($fileData);
+
+    expect($fileData)->toBeArray();
+});
+
+it('builds', function () {
+    $fileData = [];
+    $builder = app(ConfigurationBuilder::class);
+
+    $configurations = $builder->build($fileData);
+
+});
